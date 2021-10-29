d33219d7f8dd683b18159a5cf7008a9b84faf0d7
muhammad <mohamedgk5@gmail.com>
2021-10-29 17:08:54 +0200

    Add Setup server

--- commentsOnlyJS/server.js
+++ commentsOnlyJS/server.js
@@ -1,25 +1,24 @@
 // Setup empty JS object to act as endpoint for all routes
-projectData = {};
+
 // Express to run server and routes
-const express = require("express");
-const cors = require("cors");
-const bodyParser = require("body-parser");
+
 // Start up an instance of app
-const app = express();
+
 /* Dependencies */
 /* Middleware*/
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+
 //Here we are configuring express to use body-parser as middle-ware.
+
 // Cors for cross origin allowance
-app.use(cors());
+
 // Initialize the main project folder
 
 // Spin up the server
+
 // Callback to debug
 
 // Initialize all route with a callback function
-app.use(express.static("/website"));
+
 // Callback function to complete GET '/all'
 
 // Post Route
--- server.js
+++ server.js
@@ -2,18 +2,22 @@
 projectData = {};
 
 // Require Express to run server and routes
-
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 // Start up an instance of app
-
+const app = express();
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
-
+app.use(cors());
 // Initialize the main project folder
-app.use(express.static('website'));
-
+app.use(express.static("website"));
 
 // Setup Server
+app.listen(3000, () => {
+  console.log("server is runing");
+});
