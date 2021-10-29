aeb0c5a3579cb1bf5ffd0e3d2ed8164c5e638bf6
muhammad <mohamedgk5@gmail.com>
2021-10-29 20:59:29 +0200

    Add +1 to get the month number right

--- website/app.js
+++ website/app.js
@@ -2,7 +2,7 @@
 
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth() + "." + d.getDate() + "." + d.getFullYear();
+let newDate = d.getMonth() + 1 + "." + d.getDate() + "." + d.getFullYear();
 
 // Personal API Key for OpenWeatherMap API
 const apiKey = "c4249e1b8ccedb9ed46c063dea0973df";
