cb21d032bbee3a7d2251301684211793e9e98e10
muhammad <mohamedgk5@gmail.com>
2021-10-29 20:12:06 +0200

    integrating OpenWeatherMap API

--- website/app.js
+++ website/app.js
@@ -2,4 +2,21 @@
 
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth() + "." + d.getDate() + "." + d.getFullYear();
+
+// Personal API Key for OpenWeatherMap API
+const apiKey = "c4249e1b8ccedb9ed46c063dea0973df";
+const generate = document.querySelector("#generate");
+
+// Event listener to add function to existing HTML DOM element
+
+/* Function called by event listener */
+generate.addEventListener("click", async () => {
+  const zipCode = document.querySelector("#zip").value;
+  const theUrl = `https://api.openweathermap.org/data/2.5/weather?zip=${zipCode}&appid=${apiKey}&units=metric`;
+
+  const res = await fetch(theUrl);
+  const data = await res.json();
+  const temp = data.main.temp;
+  console.log(temp);
+});
